bdfbaace01fa9ce650ec57b74c7b12aba16fb4a8
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-30 23:06:39 +0000

    amended addition test and written other operator tests

--- Unit_and_Integration_Task_B/js_calculator_with_unit_tests/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_with_unit_tests/tests/unit/calculator_spec.js
@@ -11,12 +11,36 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-  it('can add two numbers', function(){
-     calculator.numberClick(1);
-     calculator.operatorClick('+');
-     calculator.numberClick(4);
-     calculator.operatorClick('=');
-     assert.equal(calculator.previousTotal, 5)
+  // it('orig can add two numbers', function(){
+  //    calculator.numberClick(1);
+  //    calculator.operatorClick('+');
+  //    calculator.numberClick(4);
+  //    calculator.operatorClick('=');
+  //    assert.equal(calculator.previousTotal, 5)
+  // })
+
+  it('can add a number', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  it('can subtract a number', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  it('can multiply numbers', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.equal(calculator.runningTotal, 15)
+  })
+
+  it('can divide numbers', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.equal(calculator.runningTotal, 3)
   })
 
 });
