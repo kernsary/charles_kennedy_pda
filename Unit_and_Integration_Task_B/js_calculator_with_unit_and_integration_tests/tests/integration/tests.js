96c8f7415ab86794cca96dde547c8ca6eecde084
Charles Kennedy <charlesscottkennedy@googlemail.com>
2020-01-02 21:49:43 +0000

    passed multiple operations inegration test

--- Unit_and_Integration_Task_B/js_calculator_with_unit_and_integration_tests/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_with_unit_and_integration_tests/tests/integration/tests.js
@@ -32,4 +32,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
+  it('should update the running total correctly after multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('20')
+  })
+
 });
