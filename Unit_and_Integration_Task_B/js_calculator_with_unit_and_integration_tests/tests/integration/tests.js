6f48a0bd82e8219f74a2278c07a1233af5101c41
Charles Kennedy <charlesscottkennedy@googlemail.com>
2020-01-02 21:45:07 +0000

    number and operator key integration tests passed

---
+++ Unit_and_Integration_Task_B/js_calculator_with_unit_and_integration_tests/tests/integration/tests.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should update the running total when a number button is pressed', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('23')
+  })
+
+  it('should update the running total after an operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
+});
